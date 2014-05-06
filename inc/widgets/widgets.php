f32a7ae02c4a43c0cfd06d29da74d2c6f7751170
Élie Michel <elie.michel@ens.fr>
2014-05-06 16:31:27 +0200

    Début de widget parallax

--- inc/widgets/widgets.php
+++ inc/widgets/widgets.php
@@ -163,6 +163,7 @@ function spacious_widgets_init() {
 	register_widget( "spacious_call_to_action_widget" );
 	register_widget( "spacious_testimonial_widget" );
 	register_widget( "spacious_recent_work_widget" );	
+	register_widget( "spacious_cw_parallax_widget" );
 }
 
 /****************************************************************************************/
@@ -645,6 +646,70 @@ class spacious_testimonial_widget extends WP_Widget {
 		<?php echo $after_widget;
  		}
  	}
+
+/**************************************************************************************/
+
+
+/**
+ * Prallax widget to show paragraphs with parallax effect (CitizenWatt addition).
+ */
+ class spacious_cw_parallax_widget extends WP_Widget {
+ 	function spacious_cw_parallax_widget() {
+ 		$widget_ops = array( 'classname' => 'widget_cw_parallax', 'description' => __( 'Show a paragraph with parallax effect.', 'spacious' ) );
+		$control_ops = array( 'width' => 200, 'height' =>250 ); //?
+		parent::WP_Widget( false, $name = __( 'CW: Parallax', 'spacious' ), $widget_ops, $control_ops);
+ 	}
+
+ 	function form( $instance ) {
+ 		$att_defaults['title'] = '';
+ 		$att_defaults['text'] = '';
+ 		$instance = wp_parse_args( (array) $instance, $att_defaults );
+		$title = esc_attr( $instance[ 'title' ] );
+		$text = esc_textarea($instance['text']);
+		?>
+	
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e( 'Title:', 'spacious' ); ?></label> 
+			<input id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+		</p>
+		<?php _e( 'Text','spacious' ); ?>
+		<textarea class="widefat" rows="10" cols="20" id="<?php echo $this->get_field_id('text'); ?>" name="<?php echo $this->get_field_name('text'); ?>"><?php echo $text; ?></textarea>
+		<?php
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance[ 'title' ] = strip_tags( $new_instance[ 'title' ] );
+		if ( current_user_can('unfiltered_html') )
+			$instance['text'] =  $new_instance['text'];
+		else
+			$instance['text'] = stripslashes( wp_filter_post_kses( addslashes($new_instance['text']) ) ); // wp_filter_post_kses() expects slashed
+		$instance['filter'] = isset($new_instance['filter']);
+
+		return $instance;
+	}
+
+	function widget( $args, $instance ) {
+ 		extract( $args );
+ 		extract( $instance );
+
+ 		global $post;
+ 		$title = isset( $instance[ 'title' ] ) ? $instance[ 'title' ] : '';
+ 		$text = apply_filters( 'widget_text', empty( $instance['text'] ) ? '' : $instance['text'], $instance );
+		echo $before_widget;
+		?>
+			<div class="parallax">
+			<div class="parallax-content">
+				<?php
+				if ( !empty( $title ) ) { echo $before_title . esc_html( $title ) . $after_title; } ?>
+				<p><?php echo esc_textarea( $text ); ?></p>
+			</div>
+			</div>
+		<?php echo $after_widget;
+ 		}
+ 	}
+
+
 
 /**************************************************************************************/
 ?>
