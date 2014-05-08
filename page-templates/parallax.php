88eed113f8b172879382b0c320ab99c94e34dc42
Élie Michel <elie.michel@ens.fr>
2014-05-08 21:56:50 +0200

    Ajout de themes à la page parallax

--- page-templates/parallax.php
+++ page-templates/parallax.php
@@ -18,6 +18,8 @@ foreach (array('One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven') as $num) {
 	$lower = strtolower($num);
 	$background_image = of_get_option( 'citizenwatt_parallax_image_'.$lower , '' );
 	$title = of_get_option( 'citizenwatt_parallax_title_'.$lower , '' );
+	$theme = of_get_option( 'citizenwatt_parallax_theme_'.$lower , '' );
+	$theme = $theme == 'default' ? '' : ' parallax-theme-' . $theme;
 	$color = of_get_option( 'citizenwatt_parallax_color_'.$lower , '' );
 	$r = hexdec(substr($color, 1, 2));
 	$g = hexdec(substr($color, 3, 2));
@@ -26,10 +28,10 @@ foreach (array('One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven') as $num) {
 	if( is_active_sidebar( 'citizenwatt_parallax_slide_' . $lower ) ) {
 ?>
 	<!-- Parallax slider <?=$lower?>-->
-	<div class="parallax" style="background-image: url(<?= esc_url( $background_image ); ?>)">
+	<div class="parallax<?=$theme?>" style="background-image: url(<?= esc_url( $background_image ); ?>)">
 		<div class="parallax-content" style="background-color: <?php echo "rgba($r,$g,$b,$a)" ?>">
 			<?php
-			if ( !empty( $title ) ) { echo '<h3>' . esc_html( $title ) . '</h3>'; }
+			if ( !empty( $title ) ) { echo '<h2 class="parallax-title">' . esc_html( $title ) . '</h2>'; }
 			// Calling the slide one of the parallax page if it exists.
 			if ( !dynamic_sidebar( 'citizenwatt_parallax_slide_' . $lower ) ):
 			endif;
