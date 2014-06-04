3f6fbc7c764ea59f8c7ca9eb57db7ad7bc11d923
Phyks <webmaster@phyks.me>
2014-06-04 19:56:10 +0200

    Theme used for launch

--- content.php
+++ content.php
@@ -51,8 +51,10 @@
 	<?php if ( 'post' == get_post_type() ) : ?>
 		<footer class="entry-meta-bar clearfix">	        			
 			<div class="entry-meta clearfix">
+				<span class="read-more-link"><a class="read-more" href="<?php the_permalink(); ?>"><?php _e( 'Lire la suite', 'spacious' ); ?></a></span>
+				<div class="right">
 				<span class="by-author"><a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a></span>
-				<span class="date"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( get_the_time() ); ?>"><?php the_time( get_option( 'date_format' ) ); ?></a></span>
+				<span class="date"><?php the_time( get_option( 'date_format' ) ); ?></span>
 				<?php if( has_category() ) { ?>
 	       		<span class="category"><?php the_category(', '); ?></span>
 	       	<?php } ?>
@@ -60,7 +62,7 @@
 	       		<span class="comments"><?php comments_popup_link( __( 'No Comments', 'spacious' ), __( '1 Comment', 'spacious' ), __( '% Comments', 'spacious' ), '', __( 'Comments Off', 'spacious' ) ); ?></span>
 	       	<?php } ?>
 	       	<?php edit_post_link( __( 'Edit', 'spacious' ), '<span class="edit-link">', '</span>' ); ?>
-				<span class="read-more-link"><a class="read-more" href="<?php the_permalink(); ?>"><?php _e( 'Read more', 'spacious' ); ?></a></span>
+				</div>
 			</div>
 		</footer>
 	<?php endif; ?>
